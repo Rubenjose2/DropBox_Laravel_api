fb61969af3fb0920a6475ffd8ca9178a69096137
Ruben Castro <ruben.jose.castro@gmail.com>
2018-12-07 16:19:39 -0500

    This is a good working version with progress bar

--- public/js/dropzone-config.js
+++ public/js/dropzone-config.js
@@ -1,7 +1,20 @@
 var token =  $('#csrf_token').attr('value');
+var total_photo_counter = 0;
+var bulksize = 0;
+
+//This function would convert the size to the conventional name size
+
+function bytesToSize(bytes) {
+    var sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB'];
+    if (bytes == 0) return '0 Byte';
+    var i = parseInt(Math.floor(Math.log(bytes) / Math.log(1024)));
+    return Math.round(bytes / Math.pow(1024, i), 2) + ' ' + sizes[i];
+};
+
 
 Dropzone.options.myDropzone= {
     url: "upload/dropzone",
+    maxFiles:30,
     timeout:180000,
     parallelUploads:1,
     acceptedFiles: 'image/*',
@@ -22,16 +35,45 @@ Dropzone.options.myDropzone= {
             e.preventDefault();
             e.stopPropagation();
             myDropzone.processQueue();
+        });
+
+        this.on("addedfile",function(file){
+            //Adding the number of files to be added into the upload
+            total_photo_counter++;
+            $("#counter").text(total_photo_counter);
+
+            //Showing the total amount of MB in the bulk process
+            bulksize = bulksize + file.size;
+            $("#bulk_size").text(bytesToSize(bulksize));
+
+            //This would enable the button in order to submit the pictures
+            $('#submit-all').removeClass('disabled');
+
+
         })
         this.on("sending",function (file,xhr,formData) {
             formData.append('school_name',jQuery('#school').val());
             formData.append("_token", token);
             formData.append("filezzide",file.size);
 
-        })
+        });
         this.on("success",function()
         {
             myDropzone.options.autoProcessQueue = true;
+        });
+
+        this.on("complete",function(file)
+        {
+            // myDropzone.removeFile(file);
+        })
+
+        this.on("totaluploadprogress",function(totalPercentage)
+        {
+            if(totalPercentage){
+                $("#myprogressbar").css('width',totalPercentage +'%');
+                console.log(totalPercentage);
+            }
+
         })
     }
 };
--- resources/views/partials/drop.blade.php
+++ resources/views/partials/drop.blade.php
@@ -6,7 +6,16 @@
                 <form action="{{route('dropzone')}}" method="post" enctype="multipart/form-data">
                     <input type="hidden" name="_token" id="csrf_token" value ="{{csrf_token()}}"/>
                     <legend>File Uploader Dropbox</legend>
+                    <div class="panel panel-default">
+                        <div class="panel-heading">
+                            <h3 class="panel-title">Instructions</h3>
+                        </div>
+                        <div class="panel-body">
+                            <strong>Number of pictures allow: </strong> 30<br>
+                            <small>Please avoid to load blurring pictures</small> <br/>
 
+                        </div>
+                    </div>
                     <div class="form-group">
                         <label for="">Please select the Game </label>
                         <div class="form-group">
@@ -21,7 +30,24 @@
 
                     <div class="dropzone" id="myDropzone"></div>
                     <hr>
-                    <button type="submit" class="btn btn-primary" id="submit-all">Submit</button>
+                    <div class="panel panel-default">
+                        <div class="panel-heading">
+                            <h3 class="panel-title">Pictures Bulk information</h3>
+                        </div>
+                        <div class="panel-body">
+                            <strong>Total Amount of Pictures to be upload: </strong> <span id="counter"></span><br/>
+                            <strong>Total size of the Bulk: </strong><span id="bulk_size"></span><br>
+
+                            {{--Progress Bar--}}
+
+                            <div class="progress">
+                                <div id="myprogressbar" class="progress-bar progress-bar-striped active" role="progressbar" aria-valuenow="45" aria-valuemin="0" aria-valuemax="100" style="width: 0%">
+                                    <span class="sr-only">45% Complete</span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <button type="submit" class="btn btn-primary disabled" id="submit-all">Submit</button>
 
 
                 </form>
